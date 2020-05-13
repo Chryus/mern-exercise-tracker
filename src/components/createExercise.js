d06cc75da066ce45e4293efbdead729cfdb78b0c
Chris Haack <chris.haack@me.com>
2020-05-13 11:51:55 -0400

    build createExercise and createUser components; refactor with react hooks

--- src/components/createExercise.js
+++ src/components/createExercise.js
@@ -1,11 +1,84 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
-export default class CreateExercise extends Component {
-  render() {
-    return (
-      <div>
-        <p>You are on the Create Exercise component!</p>
-      </div>
-    )
+const CreateExercise = function () {
+  const [users, setUsers] = useState(['test user']);
+
+	const [username, setUsername] = useState("test user");
+	const [description, setDescription] = useState("");
+	const [duration, setDuration] = useState(0);
+	const [date, setDate] = useState(new Date());
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    const exercise = {
+      username: username,
+      description: description,
+      duration: duration,
+      date: date,
+    };
+
+    console.log(exercise);
+    window.location = '/';
   }
-}
+
+  return (
+    <div>
+      <h3>Create New Exercise Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+              required
+              className="form-control"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}>
+              {
+                users.map(function(user) {
+                  return <option
+                    key={user}
+                    value={user}>{user}
+                    </option>;
+                })
+              }
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              />
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+              type="text"
+              className="form-control"
+              value={duration}
+              onChange={(e) => setDuration(e.target.value)}
+              />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker
+              selected={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateExercise
--- src/components/createUser.js
+++ src/components/createUser.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export default class CreateUser extends Component {
-  render() {
-    return (
-      <div>
-        <p>You are on the Create User component!</p>
-      </div>
-    )
+const CreateUser = function () {
+  const [username, setUsername] = useState("test user");
+
+  function onSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+      username: username,
+    };
+    console.log(newUser);
   }
+
+  return (
+    <div>
+      <h3>Create New User</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              />
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Create User" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+  )
 }
+
+export default CreateUser
